e85db2004af95d82af72cc26ed5424da30a91ed5
privetyaonton <71338577+privetyaonton@users.noreply.github.com>
2022-09-11 23:17:51 +0300

    load kisslinka lab

--- iipu_laba1.cpp
+++ iipu_laba1.cpp
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #pragma comment(lib, "setupapi.lib")
 #pragma comment(lib, "powrprof.lib")
 #include <Windows.h>
@@ -50,7 +50,43 @@ int main()
 
 void GetTypeBattery()
 {
-	HDEVINFO DeviceInfoSet = SetupDiGetClassDevsA(&GUID_DEVCLASS_BATTERY, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+	HDEVINFO DeviceInfoSet;
+	DeviceInfoSet = SetupDiGetClassDevs(&GUID_DEVCLASS_BATTERY, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+
+	SP_DEVICE_INTERFACE_DATA DeviceInterfaceData = { 0 };
+	ZeroMemory(&DeviceInterfaceData, sizeof(SP_DEVINFO_DATA));
+	DeviceInterfaceData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+	SetupDiEnumDeviceInterfaces(DeviceInfoSet, NULL, &GUID_DEVCLASS_BATTERY, 0, &DeviceInterfaceData);
+	DWORD cbRequired = 0;
+
+	SetupDiGetDeviceInterfaceDetail(DeviceInfoSet, &DeviceInterfaceData, NULL, NULL, &cbRequired, NULL);
+	PSP_DEVICE_INTERFACE_DETAIL_DATA pdidd = (PSP_DEVICE_INTERFACE_DETAIL_DATA)LocalAlloc(LPTR, cbRequired);
+	pdidd->cbSize = sizeof(*pdidd);
+
+	SetupDiGetDeviceInterfaceDetail(DeviceInfoSet, &DeviceInterfaceData, pdidd, cbRequired, &cbRequired, NULL);
+	HANDLE hBattery = CreateFile(pdidd->DevicePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	BATTERY_QUERY_INFORMATION BatteryQueryInformation = { 0 };
+	DWORD bytesWait = 0;
+	DWORD bytesReturned = 0;
+	DeviceIoControl(hBattery, IOCTL_BATTERY_QUERY_TAG, &bytesWait, sizeof(bytesWait), &BatteryQueryInformation.BatteryTag,
+		sizeof(BatteryQueryInformation.BatteryTag), &bytesReturned, NULL) && BatteryQueryInformation.BatteryTag;
+
+	BATTERY_INFORMATION BatteryInfo = { 0 };
+	BatteryQueryInformation.InformationLevel = BatteryInformation;
+
+	DeviceIoControl(hBattery, IOCTL_BATTERY_QUERY_INFORMATION, &BatteryQueryInformation, sizeof(BatteryQueryInformation),
+		&BatteryInfo, sizeof(BatteryInfo), &bytesReturned, NULL);
+
+	for (int b = 0; b < 4; b++)
+	{
+		cout << BatteryInfo.Chemistry[b];
+	};
+
+	LocalFree(pdidd);
+	SetupDiDestroyDeviceInfoList(DeviceInfoSet);
+	/* HDEVINFO DeviceInfoSet = SetupDiGetClassDevsA(&GUID_DEVCLASS_BATTERY, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
 	if (DeviceInfoSet != INVALID_HANDLE_VALUE)
 	{
 		SP_DEVICE_INTERFACE_DATA DeviceInterfaceData = { 0 };
@@ -94,7 +130,7 @@ void GetTypeBattery()
 
 		LocalFree(DeviceInterfaceDetailData);
 		SetupDiDestroyDeviceInfoList(DeviceInfoSet);
-	}
+	}*/
 }
 
 void thread_func()
